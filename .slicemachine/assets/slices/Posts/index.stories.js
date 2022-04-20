f92de87859f9f9f33370bdb5c562f9ae7d8304b8
Leandro Delinger <leandro.delinger@gmail.com>
2022-04-20 18:47:03 -0300

    Finishing

---
+++ .slicemachine/assets/slices/Posts/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Posts';
+
+export default {
+  title: 'slices/Posts'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"posts","items":[],"primary":{"title":[{"type":"heading1","text":"Matrix strategic interfaces","spans":[]}],"description":[{"type":"paragraph","text":"Cupidatat minim adipisicing aliqua velit duis sint officia anim adipisicing cupidatat voluptate reprehenderit eu voluptate enim. Ad veniam duis cillum tempor ex sunt nulla incididunt qui cupidatat labore dolore.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
